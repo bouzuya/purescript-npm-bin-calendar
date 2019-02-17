e62bcbba1ff097a85878e617c2cd20c20539829e
bouzuya <m@bouzuya.net>
2019-02-17 14:17:57 +0900

    show calendar

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,30 @@ module Main where
 
 import Prelude
 
+import Calendar (calendarDates)
+import Data.Date as Date
+import Data.Enum (toEnum)
+import Data.Foldable as Foldable
+import Data.Maybe (maybe)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console as Console
+import Effect.Exception (throw)
+import Format as Format
+import WeekDate as WeekDate
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  year <- maybe (throw "invalid year") pure (toEnum 2019)
+  calendar <- maybe (throw "invalid calendar") pure (calendarDates year)
+  Foldable.for_ calendar (Console.log <<< buildLine year)
+  where
+    buildLine year (Tuple dow wdates) =
+      Foldable.intercalate
+        " "
+        [ Format.dayOfWeekShortName dow
+        , Foldable.fold (map (buildChar year) wdates)
+        ]
+    buildChar year wdate =
+      let date = WeekDate.toDate wdate
+      in if Date.year date == year then "_" else " "
