375e05c73a39bb2c06bbd716ba3c0180778d8f5d
bouzuya <m@bouzuya.net>
2019-02-26 19:22:25 +0900

    read stdin

--- src/Main.purs
+++ src/Main.purs
@@ -6,39 +6,51 @@ import Bouzuya.DateTime.WeekDate as WeekDate
 import Calendar as Calendar
 import Data.Array as Array
 import Data.Date as Date
-import Data.Enum as Enum
 import Data.Foldable as Foldable
 import Data.Maybe (Maybe(..), maybe)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Console as Console
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Class (liftEffect)
+import Effect.Class.Console as Console
 import Effect.Exception (throw)
 import Effect.Now as Now
+import Effect.Ref as Ref
 import Foreign.Object (Object)
 import Foreign.Object as Object
 import Format as Format
 import Node.Encoding as Encoding
-import Node.FS.Sync as FS
 import Node.Process as Process
+import Node.Stream (Readable)
+import Node.Stream as Stream
 import Simple.JSON as SimpleJSON
 
+readFromStream :: Readable () -> Aff String
+readFromStream r = Aff.makeAff \callback -> do
+  ref <- Ref.new ""
+  Stream.onDataString r Encoding.UTF8 \s -> do
+    buffer <- Ref.read ref
+    Ref.write (buffer <> s) ref
+  Stream.onEnd r do
+    buffer <- Ref.read ref
+    callback (pure buffer)
+  pure mempty
+
 main :: Effect Unit
-main = do
-  args <- map (Array.drop 2) Process.argv
-  file <-
-    maybe
-      (throw "Usage: purescript-npm-bin-calendar <FILE>")
-      pure
-      (Array.head args)
-  text <- FS.readTextFile Encoding.UTF8 file
+main = Aff.launchAff_ do
+  _ <- liftEffect (map (Array.drop 2) Process.argv) -- TODO
+  text <- readFromStream Process.stdin
   calendarData <-
-    maybe
-      (throw "invalid file format")
-      pure
-      (SimpleJSON.readJSON_ text :: _ (Object Boolean))
-  year <- map Date.year Now.nowDate
+    liftEffect
+      (maybe
+        (throw "invalid file format")
+        pure
+        (SimpleJSON.readJSON_ text :: _ (Object Boolean)))
+  year <- liftEffect (map Date.year Now.nowDate)
   calendar <-
-    maybe (throw "invalid calendar") pure (Calendar.calendarDates year)
+    liftEffect
+      (maybe (throw "invalid calendar") pure (Calendar.calendarDates year))
   Foldable.for_ calendar (Console.log <<< (buildLine calendarData year))
   where
     buildLine calendarData year (Tuple dow wdates) =
